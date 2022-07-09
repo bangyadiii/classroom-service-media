340b0e5f3ceb2e9d44657184f317eca62f8b71ad
triadi <triadilaksamana@gmail.com>
2022-07-09 10:51:38 +0700

    init project

---
+++ app.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+var mediaRouter = require("./routes/media");
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/media", mediaRouter);
+
+module.exports = app;
